7c6bed128bf2336adb5c1da308e44e097613f683
kiettran <tranhatuankiet1709@gmail.com>
2023-12-23 17:25:27 +0700

    Update package.json

--- index.ts
+++ index.ts
@@ -1,13 +1,11 @@
 import * as http from 'http';
 import 'reflect-metadata';
 import App from './src/app';
-import { PrismaClient } from '@prisma/client';
 
-const port = process.env.PORT || 3080;
+const port = process.env.PORT ?? 3080;
 
 App.set('port', port);
 const server = http.createServer(App);
-const prismaClient = new PrismaClient()
 
 server.listen(port, () => {
   console.log('Server is listening on ' + port);
